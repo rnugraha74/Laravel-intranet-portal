9248002054ffdf3de38d09c519d211287b28a9d3
Reza Nugraha <rnugraha74@gmail.com>
2017-03-16 15:53:28 +0700

    Setting router

--- routes/web.php
+++ routes/web.php
@@ -16,6 +16,15 @@ Route::get('/', function () {
 });
 
 Auth::routes();
-
+Route::get('admin_login','AdminAuth\LoginController@showLoginForm')
+Route::post('admin_login','AdminAuth\LoginController@login');
+Route::post('admin_logout','AdminAuth\LoginController@logout');
+Route::post('admin_password/email','AdminAuth\ForgotPasswordController@sendResetLinkEmail');
+Route::post('admin_password/reset','AdminAuth\ForgotPasswordController@showLinkRequestForm');
+Route::post('admin_password/reset','AdminAuth\ResetPasswordController@reset');
+Route::get('admin_password/reset/{token}','AdminAuth\ResetPasswordController@showResetForm');
+Route::get ('admin_register','AdminAuth\RegisterController@showRegistrationForm');
+Route::post('admin_register','AdminAuth\RegisterController@register');
+admin_
 Route::get('/home', 'HomeController@index');
 Route::get('/admin_home', 'AdminHomeController@index');
