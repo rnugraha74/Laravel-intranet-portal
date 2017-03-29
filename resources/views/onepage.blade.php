b5fa092852db60b607908ada9f280a045841dd23
Reza Nugraha <rnugraha74@gmail.com>
2017-03-29 16:27:24 +0700

    Add new welcome page

---
+++ resources/views/onepage.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="{{ config('app.locale') }}">
+<head>
+	<meta charset="utf-8">
+	@include('partials.cms-head-meta')
+
+	<title></title>
+
+	<!-- Tell the browser to be responsive to screen width -->
+	@include('partials.cms-head-meta')
+
+	<!-- Styles -->
+	@include('partials.cms-head-styles')
+
+	<!-- Scripts -->
+	@include('partials.cms-head-scripts')
+</head>
+<body>
+
+</body>
+</html>
