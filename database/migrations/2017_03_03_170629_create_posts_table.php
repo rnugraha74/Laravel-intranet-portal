37a4623bfdb2b1a647ccc8070c857a423df2ad98
rnugraha74 <rnugraha74@gmail.com>
2017-04-11 23:54:51 +0700

    Creating post module

--- database/migrations/2017_03_03_170629_create_posts_table.php
+++ database/migrations/2017_03_03_170629_create_posts_table.php
@@ -15,7 +15,8 @@ class CreatePostsTable extends Migration
     {
         Schema::create('posts', function (Blueprint $table) {
             $table->increments('id');
-            $table->integer('author_id')->references('id')->on('users')->onDelete('restrict');
+            $table->integer('author_id')->unsigned();
+            $table->foreign('author_id')->references('id')->on('users')->onDelete('restrict');
             $table->string('title');
             $table->string('slug')->unique();
             $table->text('excerpt');
