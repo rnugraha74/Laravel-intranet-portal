a850d9d59f86ec15bbc0e8bc7eca8ab820c08477
Reza Nugraha <rnugraha74@gmail.com>
2017-05-05 18:56:38 +0700

    Working aroung on user dashboard

--- resources/views/dashboard-home.blade.php
+++ resources/views/dashboard-home.blade.php
@@ -6,20 +6,33 @@
     <!-- Content Header (Page header) -->
     <section class="content-header">
       <h1>
-        Page Header
-        <small>Optional description</small>
+        Dashboard <br>
+        <small>Please select any of your activities below.</small>
       </h1>
       <ol class="breadcrumb">
-        <li><a href="#"><i class="fa fa-dashboard"></i> Level</a></li>
-        <li class="active">Here</li>
+        <li><a href="#"><i class="fa fa-dashboard"></i>Home</a></li>
+        <li class="active">Dashboard</li>
       </ol>
     </section>
 
     <!-- Main content -->
     <section class="content">
-
-      <!-- Your Page Content Here -->
-
+      <div class="row">
+        <div class="col-sm-8">
+          <div class="box box-widget">
+            <div class="box-header with-border">
+              <h3 class="box-title">Latest News</h3>
+            </div>
+            <div class="box-body">
+              <p>This is news</p>
+            </div>
+          </div>
+        </div>
+        <div class="col-sm-4">
+          <div class="box box-widget">
+          </div>
+        </div>      
+      </div>
     </section>
     <!-- /.content -->
   </div>
